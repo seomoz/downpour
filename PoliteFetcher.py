17ff850975285fae90c895db3d53d6a727df277c
Dan Lecocq <dan@seomoz.org>
2011-10-03 15:41:20 -0700

    Relying more heavily on redis, at a lower level.

--- PoliteFetcher.py
+++ PoliteFetcher.py
@@ -4,9 +4,9 @@
 
 from downpour import BaseFetcher, logger, reactor
 
+import qr
 import time						# For to implement politeness
 import urlparse					# To efficiently parse URLs
-from collections import deque	# Thread-safe queue
 
 class PoliteFetcher(BaseFetcher):
 	# How long should we wait before making a request to the same tld?
@@ -14,95 +14,92 @@ class PoliteFetcher(BaseFetcher):
 	# Infinity, because there's not another easy way to access it?
 	infinity = float('Inf')
 	
-	def __init__(self, poolSize=10, urls=None):
+	def __init__(self, poolSize=10):
 		# Call the parent constructor
-		super(PoliteFetcher,self).__init__(poolSize)
-		self.plds = {}
-		self.requests = deque()
-		self.extend([] if urls == None else urls)
+		BaseFetcher.__init__(self, poolSize)
+		self.plds = dict((q.key, q) for q in qr.Queue.all('domain:*'))
+		self.pldQueue = qr.Queue('plds')
+		self.requests = qr.Queue('requests')
 		self.timer = None
+	
+	def __len__(self):
+		return len(self.pldQueue) + len(self.requests)
 		
 	def getKey(self, req):
 		# This actually considers the whole domain name, including subdomains, uniquely
 		# This aliasing is just in case we want to change that scheme later, easily
-		return urlparse.urlparse(req.url.strip()).hostname
+		return 'domain:%s' % urlparse.urlparse(req.url.strip()).hostname
 	
 	# Event callbacks
 	def onDone(self, request):
-		#logger.debug('Done with %s' % request.url)
 		key = self.getKey(request)
-		if len(self.plds[key]):			
+		if len(self.plds[key]):
 			delay = time.time() + self.wait
-			self.requests.append((delay, key))
+			self.pldQueue.push((delay, key))
 		else:
 			del self.plds[key]
 	
 	#################
 	# Insertion to our queue
 	#################
-	def extend(self, request):
+	def extend(self, max):
+		count = 0
 		t = time.time()
-		self.remaining += len(request)
-		for r in request:
+		r = self.requests.pop()
+		while r and count < max:
+			count += 1
 			key = self.getKey(r)
 			try:
-				self.plds[key].append(r)
+				logger.debug('Read request %s' % r.url)
+				self.plds[key].push(r)
 			except KeyError:
-				self.plds[key] = deque([r])
-				self.requests.append((t, key))
+				logger.debug('Made queue for %s' % key)
+				q = qr.Queue(key)
+				q.push(r)
+				self.plds[key] = q
+				self.pldQueue.push((t, key))
+			r = self.requests.pop()
+		self.remaining += count
 		
 	def pop(self):
-		'''Get the next (url, success, error) tuple'''
+		'''Get the next request'''
+		if len(self.requests) < self.poolSize:
+			self.extend(10000)
 		now = time.time()
 		while True:
 			# Get the next plds we might want to fetch from
-			try:
-				next = self.requests.popleft()
-			except IndexError:
-				return None
+			next = self.pldQueue.peek()
 			# If the next-fetchable is not soon enough, then wait
 			if next[0] > now:
-				self.requests.appendleft(next)
 				# If we weren't waiting, then wait
 				if self.timer == None:
 					logger.debug('Waiting %f seconds' % (next[0] - now))
 					self.timer = reactor.callLater(next[0] - now, self.serveNext)
 				return None
 			else:
+				# Go ahead and pop this item
+				next = self.pldQueue.pop()
 				# Unset the timer
 				self.timer = None
 				try:
-					return self.plds[next[1]].popleft()
-				except IndexError:
-					# This should never happen
-					logger.error('Popping from an empty pld: %s' % next[1])
-					return None
+					return self.plds[next[1]].pop()
 				except ValueError:
-					# Nor should this ever happen
+					# This should never happen
 					logger.error('Tried to pop from non-existent pld: %s' % next[1])
 					return None
 		return None
-	
-	def download(self, req):
-		'''Queue a (url, success, error) tuple request'''
-		self.remaining += 1
-		key = self.getKey(req)
-		try:
-			self.plds[key].append(req)
-		except KeyError:
-			self.plds[key] = deque([req])
-			self.requests.append((time.time(), key))
-	
+		
 if __name__ == '__main__':
-	from downpour import BaseRequest
 	import logging
+	from downpour import BaseRequest
+	
+	# Turn on logging
 	logger.setLevel(logging.DEBUG)
+
+	q = qr.Queue('requests')
+	with file('urls.txt') as f:
+		for line in f:
+			q.push(BaseRequest(line.strip()))
 	
-	f = file('urls.txt')
-	reqs = [BaseRequest(u) for u in f.read().strip().split('\n')]
-	f.close()
-	
-	#urls = ['http://en.wikipedia.org/wiki/A', 'http://en.wikipedia.org/wiki/B', 'http://en.wikipedia.org/wiki/C', 'http://google.com']
-	#reqs = [BaseRequest(u) for u in urls]
-	p = PoliteFetcher(100, reqs)
+	p = PoliteFetcher(100)
 	p.start()
